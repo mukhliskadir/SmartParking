84acb07a7b2428ec914f8f7fc53f176fbfbda374
MUHAMMAD MUKHLIS BIN AB KADIR <mukhliskadir2207@gmail.com>
2024-02-26 10:22:03 +0800

    update icon

--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -104,7 +104,7 @@
                         <div
                             class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                             <i style="font-size: 1rem;"
-                                class="fas fa-lg fa-map ps-2 pe-2 text-center
+                                class="fas fa-lg fa-clipboard ps-2 pe-2 text-center
                         {{ in_array(request()->route()->getName(), ['manage-reservation']) ? 'text-white' : 'text-dark' }}"></i>
                         </div>
                         <span class="nav-link-text ms-1">Manage Reservation</span>
